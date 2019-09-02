df6166333e99d7ddc8ec42f3f306421ca86739e6
David N <bigair_1080@hotmail.com>
2019-09-02 00:00:15 -0300

    Updates

---
+++ precache-manifest.5b530775b999d2fc971fe7f4df472006.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09eb49f5faf224dd14796797dcedef3b",
+    "url": "/most-polluted-cities/index.html"
+  },
+  {
+    "revision": "75c34345c3b7283e4bec",
+    "url": "/most-polluted-cities/static/js/2.e1c794e6.chunk.js"
+  },
+  {
+    "revision": "1b75fec2e72363f7bcb3",
+    "url": "/most-polluted-cities/static/js/main.6e2a92cc.chunk.js"
+  },
+  {
+    "revision": "b2e285ccdab6db1d2cd1",
+    "url": "/most-polluted-cities/static/js/runtime~main.f4845ab7.js"
+  },
+  {
+    "revision": "24520a1bb8c4aa5f2c18cab7d7a61914",
+    "url": "/most-polluted-cities/static/media/city-pollution.24520a1b.svg"
+  },
+  {
+    "revision": "a4bf26956f0ec027853d7c4ed725ca0d",
+    "url": "/most-polluted-cities/static/media/cow-pollution.a4bf2695.svg"
+  },
+  {
+    "revision": "4a4259abd5b8198b804404132497f8d3",
+    "url": "/most-polluted-cities/static/media/industrial-pollution.4a4259ab.svg"
+  },
+  {
+    "revision": "f015ba140755f7b7a8622179974c014f",
+    "url": "/most-polluted-cities/static/media/masked-man.f015ba14.svg"
+  }
+]);
